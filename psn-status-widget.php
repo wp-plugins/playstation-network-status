94826fb1b9ac53f82ea56f937d6bfedfa967428b
niraj.shah <niraj.shah@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-12-28 23:12:27 +0000

    Updated to version v0.2 - Added region selection
    git-svn-id: https://plugins.svn.wordpress.org/playstation-network-status/trunk@481675 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- psn-status-widget.php
+++ psn-status-widget.php
@@ -5,7 +5,7 @@ Plugin Name: PSN Status Widget
 Plugin URI: http://www.webniraj.com/wordpress/
 Description: Displays the status of the PlayStation Network on your blog in real time. This plugin is a widget you can place in the sidebar.
 Author: Niraj Shah
-Version: 0.1
+Version: 0.2
 Author URI: http://www.webniraj.com/
 */
 
@@ -20,7 +20,7 @@ function widget_psnstatus_register() {
 				echo $before_widget;
 				echo $before_title . $options['title'] . $after_title;
 				echo '<div class="psn-status-wrapper">'
-					. file_get_contents( "http://www.xtremeps3.com/psn-status/index.php" )
+					. file_get_contents( "http://psnstatus.xtremeps3.com/index.php?region=" . $options['region'] )
 					. '</div>';
 				//	please do not remove!
 				echo '<div class="psn-status-powered-by">Powered by <a href="http://www.xtremeps3.com/" target="_blank">XTREME PS3</a></div>';
@@ -49,18 +49,58 @@ function widget_psnstatus_register() {
 
 	function widget_psnstatus_control() {
 		$options = $newoptions = get_option('widget_psnstatus');
+		
 		if ( $_POST["psnstatus-submit"] ) {
 			$newoptions['title'] = strip_tags(stripslashes($_POST["psnstatus-title"]));
-			if ( empty($newoptions['title']) ) $newoptions['title'] = 'PSN Status';
+			if ( empty( $newoptions['title'] ) ) $newoptions['title'] = 'PSN Status';
+			$newoptions['region'] = $_POST["psnstatus-region"];
 		}
 		if ( $options != $newoptions ) {
 			$options = $newoptions;
-			update_option('widget_psnstatus', $options);
+			update_option( 'widget_psnstatus', $options );
 		}
 		$title = htmlspecialchars($options['title'], ENT_QUOTES);
+		
 	?>
-				<p><label for="akismet-title"><?php _e('Title:'); ?> <input style="width: 250px;" id="psnstatus-title" name="psnstatus-title" type="text" value="<?php echo $title; ?>" /></label></p>
+				<p>
+					<label for="psnsstatus-title"><?php _e('Title:'); ?>
+						<input class="widefat" id="psnstatus-title" name="psnstatus-title" type="text" value="<?php echo $title; ?>" />
+					</label>
+				</p>
+				<p>
+					<select class="widefat" id="psnstatus-region" name="psnstatus-region">
+						<option value="AT">Austria</option>
+						<option value="AU">Australia</option>
+						<option value="BE">Belgium</option> 
+						<option value="CH">Switzerland</option> 
+						<option value="CZ">Czech Republic</option> 
+						<option value="DE">Germany</option> 
+						<option value="DK">Denmark</option> 
+						<option value="ES">Spain</option> 
+						<option value="FI">Finland</option> 
+						<option value="FR">France</option> 
+						<option value="GR">Greece</option> 
+						<option value="IE">Ireland</option> 
+						<option value="IN">India</option> 
+						<option value="IT">Italy</option> 
+						<option value="LU">Luxembourg</option> 
+						<option value="NL">Netherlands</option> 
+						<option value="NO">Norway</option> 
+						<option value="NZ">New Zealand</option> 
+						<option value="PL">Poland</option> 
+						<option value="PT">Portugal</option> 
+						<option value="RU">Russia</option> 
+						<option value="SE">Sweden</option> 
+						<option value="TR">Turkey</option>
+						<option value="AE">United Arab Emirates</option>
+						<option value="UK">United Kingdom</option> 
+						<option value="ZA">South Africa</option>
+					</select>
+				</p>
 				<input type="hidden" id="psnstatus-submit" name="psnstatus-submit" value="1" />
+				<script type="text/javascript">
+					jQuery('#psnstatus-region option[value="<?php echo $options['region']; ?>"]').attr('selected', true);
+				</script>
 	<?php
 	}
 
--- readme.txt
+++ readme.txt
@@ -3,23 +3,31 @@ Contributors: niraj.shah
 Donate link:
 Tags: status, playstation, psn
 Requires at least: 2.7
-Tested up to: 2.7
-Stable tag: 0.1
+Tested up to: 3.3
+Stable tag: 0.2
 
-Widget that shows the status of the PlayStation Network on your blog.
+Widget that shows the status of the PlayStation Network on your blog for your chosen region.
 
 == Description ==
 
-This sidebar widget displays the status of the PlayStation Network on your blog. It's simple to install and no configuration is needed.
+This sidebar widget displays the status of the PlayStation Network on your blog. It's simple to install and setup. You can choose to display the PSN Status for one of 26 countries.
 
 == Installation ==
 
 1. Upload `psn-status-widget.php` to the `/wp-content/plugins/` directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
-3. Add the widget to your Sidebar and you're done!
+3. Add the widget to your Sidebar
+4. Configure the widget as required (title and region)
+5. And you're done!
 
 == Example ==
 
 You can see a examples of the widget on my blog:
 
 * http://www.xtremeps3.com/
+
+== Changelog ==
+
+v0.2:
+
+* Country selection added to the widget. You can now set the Widget to display the PSN status for one of 26 countries.
